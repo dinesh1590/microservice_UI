5c300d7f89145932076168f8ce6a2d3ca0b5fa4b
FIRST_NAME LAST_NAME <dineshreddy1590@gmail.com>
2024-06-29 11:08:02 +0530

    gg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee-m/employee/employee.component';
+import { DepartmentComponent } from './department-m/department/department.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'department',component:DepartmentComponent},
+  {path:'employee',component:EmployeeComponent},
+  {path:'',redirectTo:'/department', pathMatch:'full'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
